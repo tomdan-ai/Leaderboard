8f449a96f66db3c8a58df32135e83d01199b61f3
Tom Daniel <tomudoh258@gmail.com>
2023-04-20 11:09:17 +0100

    Use ES6 in API

--- src/modules/scoresApi.js
+++ src/modules/scoresApi.js
@@ -9,16 +9,17 @@ export const fetchData = async () => {
   return data;
 };
 
-export async function setScores(data) {
-  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/`;
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  };
-  const response = await fetch(url, options);
-  const result = await response.json();
-  return result;
-}
+const setScores = async (data) => {
+    const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/`;
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result;
+  }
+  
