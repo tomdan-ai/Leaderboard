805490aed7d07286b6d206556b6aba793c6af894
Tom Daniel <tomudoh258@gmail.com>
2023-04-20 10:54:13 +0100

    Add APIs to Leaderboard

---
+++ src/modules/scoresApi.js
@@ -0,0 +1,24 @@
+// import result from 'lodash';
+
+const key = 'EWk0pUvYwIWEyq3FIRzT';
+
+export const fetchData = async () => {
+  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+export async function setScores(data) {
+  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/`;
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(url, options);
+  const result = await response.json();
+  return result;
+}
